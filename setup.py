2e59028f7b841f68ff249f70d0d9eddf1394c01f
Lynch <VERISK\i28373@a-1jz9nkmgv6wrv.veriskdom.com>
2021-10-28 12:01:37 -0400

    Added spacing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name = 'flaskr',
     version = '1.0.0',
-    packages = find_packages,
+    packages = find_packages(),
     include_package_data = True,
     zip_safe = False,
     install_requires = [
